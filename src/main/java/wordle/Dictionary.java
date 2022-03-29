4ee38cff1d39e4b02d497c5f460327c07ae0e4f9
dkozlov <dkoz815@gmail.com>
2022-03-29 11:34:35 +0300

    Добавлены пакеты. Строки в тестах заменены на константы

--- src/main/java/wordle/Dictionary.java
+++ src/main/java/wordle/Dictionary.java
@@ -1,3 +1,5 @@
+package wordle;
+
 import java.util.List;
 
 public interface Dictionary {
--- src/main/java/wordle/Game.java
+++ src/main/java/wordle/Game.java
@@ -1,3 +1,5 @@
+package wordle;
+
 import java.util.*;
 
 public class Game {
@@ -8,8 +10,12 @@ public class Game {
         MISSING_IN_WORD
     }
 
-   public static final String GAME_WIN = "Game is win";
-   public static final String GAME_LOSE = "Game is lose";
+    public static void main(String[] args) {
+        Game game = new Game(new TxtDictionary("src/main/resources/dictionaries/dictionary.txt"), "TOAST");
+    }
+
+   public static final String GAME_WIN = "wordle.Game is win";
+   public static final String GAME_LOSE = "wordle.Game is lose";
    public static final String INCORRECT_WORD = "Incorrect word";
    public static final String INCORRECT_INPUT = "Incorrect input, try another word!";
    private static final int WORD_LENGTH = 5;
