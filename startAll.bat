1a96e5f4bffb6519c0dc37f3375e68545cf2d063
dkozlov <dkoz815@gmail.com>
2022-03-29 11:35:28 +0300

    Обновлен .bat запуска тестов.

--- startAll.bat
+++ startAll.bat
@@ -1,7 +1,9 @@
 @echo off
-set targetDirectory=target/classes/Wordle
-set sourcepath=src/main/java;src/test/java
+set targetDirectory=target/classes/
 
-javac -d %targetDirectory% -cp src/test/resources/junit-4.13.2.jar -sourcepath %sourcepath% src/test/java/*.java
+set sourcepath=src/main/java/
 
-java -classpath %targetDirectory%;src/test/resources/junit-4.13.2.jar GameTest
+javac -d %targetDirectory%  src/main/java/wordle/*.java
+javac -d %targetDirectory% -cp src/test/resources/junit-4.13.2.jar -sourcepath %sourcepath% src/test/java/wordleTest/*.java
+
+java -classpath %targetDirectory%;src/test/resources/junit-4.13.2.jar wordleTest.GameTest
