ab9397e38ebe489d0d7152bd53701162cae8958e
dkozlov <dkoz815@gmail.com>
2022-03-30 09:15:27 +0300

    Create simple ParametrizedGameTest

--- startAll.bat
+++ startAll.bat
@@ -7,3 +7,4 @@ javac -d %targetDirectory%  src/main/java/wordle/*.java
 javac -d %targetDirectory% -cp src/test/resources/libs/junit-4.13.2.jar -sourcepath %sourcepath% src/test/java/wordleTest/*.java
 
 java -classpath %targetDirectory%;src/test/resources/libs/junit-4.13.2.jar;src/test/resources/libs/hamcrest-core-1.3.jar  org.junit.runner.JUnitCore wordleTest.GameTest
+java -classpath %targetDirectory%;src/test/resources/libs/junit-4.13.2.jar;src/test/resources/libs/hamcrest-core-1.3.jar  org.junit.runner.JUnitCore wordleTest.ParametrizedGameTest
