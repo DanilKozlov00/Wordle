83715c5e3b865f7e6e8eb42518e62a585096d7af
Danil Kozlov <55684917+DanilKozlov00@users.noreply.github.com>
2022-05-12 12:17:47 +0300

    Update startLinux.sh

--- startLinux.sh
+++ startLinux.sh
@@ -1,4 +1,4 @@
 mkdir -p target/classes
 cp -R src/main/resources/ target/classes
-javac -d target/classes -classpath target/classes/libs/* -sourcepath src/main/java src/main/java/wordle/*.java
-java -classpath target/classes:src/main/resources/libs/* wordle.Main
+javac -d target/classes -classpath "target/classes/resources/libs/*" -sourcepath src/main/java src/main/java/wordle/model/TxtDictionary.java
+java -classpath "target/classes:target/classes/resources/libs/*" wordle.Main
